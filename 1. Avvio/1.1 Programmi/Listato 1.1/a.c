7f885954235e9b65b8540aff0bdf23c7fa1196c0
Marco Fiorillo <64414957+DestinyODIN580@users.noreply.github.com>
2021-07-03 23:18:03 +0200

    main.c

--- 1. Avvio/1.1 Programmi/Listato 1.1/a.c
+++ 1. Avvio/1.1 Programmi/Listato 1.1/a.c
@@ -1,1 +1,17 @@
+/* 1.1 - Un programma in linguaggio C */
 
+#include <stdio.h>
+
+int main (int argc, char const *argv[])
+{
+    printf ("Tre");
+    printf (" casettine");
+    printf (" dai");
+    printf (" tetti");
+    printf (" aguzzi");
+
+
+    printf ("\n");
+    return 0;
+}
+// Marco Fiorillo 3/07/2021
