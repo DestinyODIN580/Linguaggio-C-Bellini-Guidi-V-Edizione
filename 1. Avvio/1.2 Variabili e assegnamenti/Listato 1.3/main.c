7eda27edf3ee3af59c2ef4ee1b1ddd5ef25ec596
Marco Fiorillo <64414957+DestinyODIN580@users.noreply.github.com>
2021-07-03 23:26:37 +0200

    Create main.c

---
+++ 1. Avvio/1.2 Variabili e assegnamenti/Listato 1.3/main.c
@@ -0,0 +1,22 @@
+/* 1.3 - Uso di variabili */
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+    int base;
+    int altezza;
+    int area;
+
+    base = 3;
+    altezza = 7;
+    area = base * altezza;
+
+    
+    printf ("Area >> %d\n", area);
+
+
+    printf ("\n");
+    return 0;
+}
+// Marco Fioriloo 3/07/2021
