2825ca14d3631cd0e5994dd8ee8e4fa5b1a9a442
Marco Fiorillo <64414957+DestinyODIN580@users.noreply.github.com>
2021-07-03 23:15:27 +0200

    Add files via upload

---
+++ 1. Avvio/1.1 Programmi/Listato 1.1/main.c
@@ -0,0 +1,17 @@
+/* 1.2 - Una variante del programma precedente */
+
+#include <stdio.h>
+
+int main (int argc, char const *argv[])
+{
+    printf ("Tre\n");
+    printf (" casettine\n");
+    printf (" dai\n");
+    printf (" tetti\n");
+    printf (" aguzzi\n");
+
+
+    printf ("\n");
+    return 0;
+}
+// Marco Fiorillo 3/07/2021
