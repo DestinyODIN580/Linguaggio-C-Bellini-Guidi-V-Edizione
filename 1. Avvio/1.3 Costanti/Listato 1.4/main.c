3f65625960b9dea3752b7d0c875d1d506d3bd2d9
Marco Fiorillo <64414957+DestinyODIN580@users.noreply.github.com>
2021-07-03 23:43:06 +0200

    Create main.c

---
+++ 1. Avvio/1.3 Costanti/Listato 1.4/main.c
@@ -0,0 +1,25 @@
+/* 1.4 - Uso di costanti */
+
+/* Calcolo area rettangolo, prova utilizzo costanti */
+
+#include <stdio.h>
+
+#define BASE 3
+#define ALTEZZA 7
+
+int main (int argc, char const * argv[])
+{
+    int area;
+
+    area = BASE * ALTEZZA;
+
+
+    printf ("Base: %d\n", BASE);
+    printf ("Altezza: %d\n", ALTEZZA);
+    printf ("Area: %d\n", area);
+    
+
+    printf ("\n");
+    return 0;
+}
+// Marco Fiorillo 3/07/2021
