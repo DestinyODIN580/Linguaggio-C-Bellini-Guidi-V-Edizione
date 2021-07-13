d3094d0097512402ac5b89fd5a77d868d664323e
DestinyODIN580 <marco.fiorillo@outlook.com>
2021-07-13 21:43:15 +0200

    #

--- Linguaggio C (Bellini, Guidi)/6. Stringhe/6.4 Esercizi/3/main.c
+++ Linguaggio C (Bellini, Guidi)/6. Stringhe/6.4 Esercizi/3/main.c
@@ -1,9 +1,10 @@
-/* 65.
-    Scrivere un programma che, senza utilizzare la libreria string.h, concateni a una stringa i primi cinque caratteri di
-    una seconda stringa.
+/* 67.
+    Scrivere un programma che ottenga lo stesso effetto richiesto nell’Esercizio 1 ma con l’utilizzo della funzione
+    strncat della libreria string.h.
 */
 
 #include <stdio.h>
+#include <string.h>
 
 #define LEN 100
 #define N 5
@@ -13,8 +14,6 @@ int main (int argc, char const *argv[])
     char str1[LEN];     /* stringa 1 */
     char str2[LEN];     /* stringa 2 */
 
-    int str1Len;        /* lunghezza str1 */
-    int str2Len;        /* lunghezza str2 */
     int c;              /* carattere in ingresso */
     int i, j;           /* contatori */
 
@@ -25,19 +24,17 @@ int main (int argc, char const *argv[])
     printf ("Prima stringa   >> ");
     while ((c = getchar ()) != '\n')
         str1[i++] = c;
-    str1[str1Len = i] = '\0';
+    str1[i] = '\0';
 
     /* costruzione str2 */
     printf ("Seconda stringa >> ");
     i = 0;
     while ((c = getchar ()) != '\n')
         str2[i++] = c;
-    str2[str2Len = i] = '\0';
+    str2[i] = '\0';
 
-    /* append primi N caratteri da str2 a str1 */
-    for (i = str1Len, j = 0; i < (str1Len + N) && j < (str2Len); i++, j++)
-        str1[i] = str2[j];
-    str1[++i] = '\0';
+    /* append primi N caratteri da str2 a str1 tramite atrncat*/
+    strncat (str1, str2, N);
 
     /* esito */
     printf ("Primi 5 caratteri in append >> %s\n", str1);
