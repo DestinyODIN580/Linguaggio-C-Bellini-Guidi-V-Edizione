0aee0cf45f32a20752a5d797069da3e304ea0f58
DestinyODIN580 <marco.fiorillo@outlook.com>
2021-08-12 11:06:56 +0200

    #

--- Linguaggio C (Bellini, Guidi)/13. File/13.2 Lettura e scrittura su file/Listato 13.2/main.c
+++ Linguaggio C (Bellini, Guidi)/13. File/13.2 Lettura e scrittura su file/Listato 13.2/main.c
@@ -5,3 +5,29 @@
 
 #include <stdio.h>
 #include <string.h>
+
+int main (int argc, char const *argv[])
+{
+    char buf[100];      /* Buffer */
+
+    int len;            
+
+    FILE *filePointer;  /* File pointer */
+
+
+    /* Legge da tastiera il nome del fornitore */
+    printf ("Inserisci un fornitore: ");
+    scanf ("%s", buf);
+
+    len = strlen (buf);
+    filePointer = fopen ("fornitori", "w");  /* Crea il file fornitori*/
+
+    /* Memorizza il nome del fornitore nel file */
+    fwrite (buf, 1, len, filePointer);
+    fclose (filePointer);                    /* Chiude il file */
+
+
+    printf ("\n");
+    return 0;
+}
+// Marco Fiorillo 12/08/2021
