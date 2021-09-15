d372e104cc3db8c88eddf9163701b7e349812fbc
DestinyODIN580 <marco.fiorillo@outlook.com>
2021-09-15 22:33:01 +0200

    #

--- Linguaggio C (Bellini, Guidi)/13. File/13.9 Esercizi/4/main.c
+++ Linguaggio C (Bellini, Guidi)/13. File/13.9 Esercizi/4/main.c
@@ -4,4 +4,36 @@
     le due letture devono produrre il medesimo risultato.
 */
 
+#include <stdio.h>
 
+#define LEN 100
+
+int main (int argc, char const *argv[])
+{
+    char buffer[LEN];       /* buffer per la riga letta */
+
+    FILE *fp;               /* puntatore al file */
+
+    fp = fopen ("file.txt", "a+");
+
+
+    /* scan della prima riga */
+    fscanf (fp, "%s\n", buffer);
+
+    /* esito */
+    printf ("1 >> %s\n", buffer);
+
+    /* reset del puntatore a inizio file */
+    fseek (fp, 0L, SEEK_SET);
+
+    /* scan della prima riga */
+    fscanf (fp, "%s\n", buffer);
+
+    /* esito */
+    printf ("2 >> %s\n", buffer);
+
+
+    printf ("\n");
+    return 0;
+}
+// Marco Fiorillo 15/09/2021
