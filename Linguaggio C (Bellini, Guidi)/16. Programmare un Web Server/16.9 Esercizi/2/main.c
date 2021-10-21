96dff5798e1eea8a8cbc51e363104049a45212d2
DestinyODIN580 <marco.fiorillo@outlook.com>
2021-10-21 23:59:04 +0200

    #

---
+++ Linguaggio C (Bellini, Guidi)/16. Programmare un Web Server/16.9 Esercizi/2/main.c
@@ -0,0 +1,22 @@
+/* 173.
+    Realizzare un CGI che legga lo stato attuale da un file di log e lo presenti in forma di pagine HTML.
+*/
+
+#include <stdio.h>
+
+int main (int argc, char const *argv[])
+{
+    FILE *fp;   /* file da cui leggere i dati */
+
+    fp = fopen ("log.txt", "r");
+
+
+    /* esito */
+    while ((!feof (fp)))
+        fputc ("%c", fp);
+
+
+    printf ("\n");
+    return 0;
+}
+// Marco Fiorillo 21/10/2021 
